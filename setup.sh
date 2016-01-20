eca52247bad88797d0e13d17b0bd0f18ae09a75c
Daniel Underwood <daniel.underwood13@gmail.com>
2016-01-20 16:55:17 -0500

    Copy .vimrc in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Copy .vimrc to home
+if [ -e ~/.vimrc ];
+then
+	echo "$HOME/.vimrc exists. Moving to $HOME.vimrc.old"
+	mv ~/.vimrc ~/.vimrc.old
+fi
+
+echo "Copying .vimrc to $HOME"
+cp .vimrc ~
+
 # Clone vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
