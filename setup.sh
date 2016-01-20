e6736f80faf413043a7ae257042c858523a5b981
Daniel Underwood <daniel.underwood13@gmail.com>
2016-01-20 16:55:38 -0500

    Force setup script to use python 2

--- setup.sh
+++ setup.sh
@@ -17,5 +17,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # Compile YCM
-python ~/.vim/bundle/YouCompleteMe/install.py \
+python2 ~/.vim/bundle/YouCompleteMe/install.py \
 	--clang-completer --racer-completer
